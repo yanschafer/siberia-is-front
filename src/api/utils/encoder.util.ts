24d9d62975fd89a0d052805e86d1091fa49e06e4
dudosyka <a.hatson22@gmail.com>
2023-12-29 03:45:54 +0300

    feat(api): base64 file encoder util

---
+++ src/api/utils/encoder.util.ts
@@ -0,0 +1,21 @@
+class EncoderUtil {
+  public async encode(file: File): Promise<string | null> {
+    const encoded = await (new Promise<string | null>(async (resolve, _) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = () => resolve(null)
+    }))
+
+    if (encoded == null)
+      return null
+    return this.removeHeader(encoded)
+  }
+
+  private removeHeader(encoded: string): string {
+    const dataPart = encoded.split("base64")[1]
+    return dataPart.substring(1, dataPart.length)
+  }
+}
+
+export default new EncoderUtil()
