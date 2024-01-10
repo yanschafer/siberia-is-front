80e001fb19e8acb0d5162228856fe3eb57bf7901
yanschafer <design@yschafer.ru>
2024-01-10 18:17:33 +0300

    TreeDropdown partly working

--- src/components/Elements/TreeDropdownComponent.vue
+++ src/components/Elements/TreeDropdownComponent.vue
@@ -1,5 +1,21 @@
 <template>
-  <TreeSelect v-model="selectedValue" :options="nodes" placeholder="Select Item" class="md:w-20rem w-full" />
+  <TreeSelect
+      v-model="selectedValue"
+      :options="filteredNodes"
+      placeholder="Select Item"
+      :filter="true"
+      :filterBy="['label']"
+      class="md:w-20rem w-full"
+  >
+    <template #header>
+      <div class="p-inputgroup">
+        <span class="p-inputgroup-addon">
+          <i class="pi pi-search"></i>
+        </span>
+        <input v-model="searchText" type="text" class="p-inputtext" placeholder="Search">
+      </div>
+    </template>
+  </TreeSelect>
 </template>
 
 <script lang="ts">
@@ -18,16 +34,31 @@ export default {
   },
   data() {
     return {
-      selectedValue: this.nodes,
+      selectedValue: this.nodes.label,
+      searchText: '',
+      expandedKeys: [],
     };
   },
-  watch: {
-    modelValue: {
-      handler(newValue) {
-        // Вызывается при изменении значения из родительского компонента
-        console.log('New value from parent:', newValue);
-      },
-      immediate: true,
+  computed: {
+    filteredNodes() {
+      return this.filterNodes(this.nodes, this.searchText.toLowerCase(), true);
+    },
+  },
+  methods: {
+    filterNodes(nodes, searchText, parentMatch) {
+      return nodes.reduce((result, node) => {
+        const labelMatch = node.label.toLowerCase().includes(searchText);
+        const expanded = labelMatch || parentMatch;
+        const filteredChildren = this.filterNodes(node.children || [], searchText, labelMatch);
+
+        if (labelMatch) {
+          result.push({ ...node, children: filteredChildren, expanded });
+        } else if (filteredChildren.length > 0) {
+          result.push({ ...node, children: filteredChildren, expanded });
+        }
+
+        return result;
+      }, []);
     },
   },
 };
