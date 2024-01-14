cd098eaac7dc70d3b74974cab9459fe0dc3511da
yanschafer <design@yschafer.ru>
2024-01-14 20:47:39 +0300

    NotificationComponent updated

---
+++ src/components/Elements/NotificationMessageComponent.vue
@@ -0,0 +1,80 @@
+<template>
+  <transition-group name="p-message" tag="div">
+    <Message
+        class="noti-message"
+        v-if="messages && messages.length > 0"
+        :key="index"
+        v-for="(message, index) in messages"
+        :severity="message.severity"
+        :closable="true"
+        @onClose="handleClose"
+    >
+    <MDBRow style="min-width: 16rem;" class="w-100">
+      <MDBCol class="col-auto">
+        <span class="title">{{ message.summary }}</span>
+      </MDBCol>
+      <MDBCol class="col-auto">
+      </MDBCol>
+    </MDBRow>
+    <MDBRow>
+      <MDBCol class="d-flex flex-column col-auto">
+        {{ message.text }}
+        <span class="date-time">
+          {{message.dateTime}}
+        </span>
+      </MDBCol>
+    </MDBRow>
+    </Message>
+    <div class="no-notifications" v-else>
+      No notifications
+    </div>
+  </transition-group>
+</template>
+
+<script lang="ts">
+import Message from 'primevue/message';
+import { MDBRow, MDBCol } from 'mdb-vue-ui-kit';
+
+export default {
+  components: {
+    Message,
+    MDBRow,
+    MDBCol,
+  },
+  props: {
+    messages: {
+      type: Array,
+    },
+  },
+  methods: {
+    handleClose() {
+      if (this.messages.length === 0) {
+        this.op.hide();
+      }
+    },
+  }
+};
+</script>
+
+<style scoped>
+.noti-message {
+  width: 100%;
+}
+:deep(.p-message-wrapper) {
+  width: 100%;
+  min-width: min-content;
+  display: flex;
+  flex-direction: row;
+  align-content: flex-start;
+  flex-wrap: wrap;
+  align-items: flex-start;
+}
+.date-time {
+  font-weight: 400;
+  font-size: 0.8rem;
+  opacity: 0.5;
+}
+.title {
+  font-weight: 600;
+}
+</style>
--- src/components/Elements/NotificationsComponent.vue
+++ src/components/Elements/NotificationsComponent.vue
@@ -1,78 +1,94 @@
 <template>
-  <MDBDropdown align="right" v-model="dropdown">
-    <MDBDropdownToggle class="btn-dark icon-bg d-flex justify-content-center align-items-center" @click="dropdown = !dropdown">
-      <IconBellFilled :color="defaultNotification1.iconColor" :size="24" stroke-width="1" />
-    </MDBDropdownToggle>
-    <MDBDropdownMenu class="dropdown-main mt-2 dropdown-bg d-flex flex-column gap-2">
-      <MDBContainer class="d-flex overflow-y-scroll dropdown-bg flex-column gap-2">
-        <template v-for="notification in [errorNotification, warningNotification, defaultNotification1, defaultNotification2]" :key="notification.type">
-          <MDBContainer v-if="notification.visible" :class="['notification-body', notification.type === 'error' ? 'badge-danger' : (notification.type === 'warning' ? 'badge-warning' : 'alert-secondary'), 'd-flex w-100']" fluid>
-            <MDBCol class="col-auto">
-              <h5>{{ notification.title }}</h5>
-              <p class="notification-text">{{ notification.text }}</p>
-            </MDBCol>
-            <MDBCol class="d-flex flex-row justify-content-end">
-              <component :is="notification.type" :color="notification.iconColor" :size="52" stroke-width="1" />
-            </MDBCol>
-          </MDBContainer>
-          <MDBRow class="d-flex justify-content-between w-100">
-            <span class="mark-read w-auto" @click="markAsRead(notification)">Mark as read</span>
-            <span class="date-time w-auto p-0">{{ notification.dateTime }}</span>
-          </MDBRow>
-          <hr class="separator" />
-        </template>
+  <div>
+    <Button
+        type="button"
+        class="icon-bg d-flex justify-content-center align-items-center"
+        icon="pi pi-image"
+        label="Image"
+        @click="toggle">
+      <IconBellFilled
+        color="#B8B8B8"
+        :size="24"
+        stroke-width="1" />
+    </Button>
+    <OverlayPanel
+        class="notification-body"
+        ref="op">
+      <MDBContainer
+          class="noti-container overflow-y-scroll">
+        <NotificationMessageComponent
+            :op="op"
+            :messages="notificationMessages" />
       </MDBContainer>
-      <hr class="separator" />
-      <MDBRow class="noti-footer d-flex flex-row justify-content-end">
-        <MDBBtn class="utility-btn" outline="dark" @click="clearAllNotifications">CLEAR ALL</MDBBtn>
+      <MDBRow class="footer-row">
+        <Button
+          @click="clearAllMessages"
+          :disabled="notificationMessages.length === 0">
+          Clear
+        </Button>
       </MDBRow>
-    </MDBDropdownMenu>
-  </MDBDropdown>
+    </OverlayPanel>
+    <Toast ref="toast" />
+  </div>
 </template>
 
 <script lang="ts">
-import { IconAlertTriangle, IconBell, IconBellFilled, IconExclamationCircle } from "@tabler/icons-vue";
+import { IconBell, IconBellFilled, IconRoute } from "@tabler/icons-vue";
 import { MDBBtn, MDBCol, MDBContainer, MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBRow } from "mdb-vue-ui-kit";
-import { ref } from "vue";
-import { useNotificationsStore } from '@/stores/store';
+import OverlayPanel from 'primevue/overlaypanel';
+import Button from 'primevue/button';
+import Toast from "primevue/toast";
+import NotificationMessageComponent from "@/components/Elements/NotificationMessageComponent.vue";
 
 export default {
   name: 'NotificationsComponent',
   components: {
-    IconAlertTriangle,
+    NotificationMessageComponent,
+    IconRoute,
     IconBell,
     IconBellFilled,
-    IconExclamationCircle,
-    MDBBtn,
-    MDBCol,
+    MDBRow,
     MDBContainer,
-    MDBDropdown,
-    MDBDropdownMenu,
-    MDBDropdownToggle,
-    MDBRow
+    OverlayPanel,
+    Button,
+    Toast
   },
-  setup() {
-    const dropdown = ref(false);
-    const notificationsStore = useNotificationsStore();
-
-    const markAsRead = (notification) => {
-      notificationsStore.hideNotification(notification);
-    };
-
-    const clearAllNotifications = () => {
-      notificationsStore.hideAllNotifications();
-    };
-
+  data() {
     return {
-      dropdown,
-      markAsRead,
-      clearAllNotifications,
-      errorNotification: notificationsStore.errorNotification,
-      warningNotification: notificationsStore.warningNotification,
-      defaultNotification1: notificationsStore.defaultNotification1,
-      defaultNotification2: notificationsStore.defaultNotification2,
+      op: null,
+      notificationMessages: [
+        { severity: 'success', summary: 'Success Message', text: 'Initial Success Message', dateTime: "2022-01-01" },
+        { severity: 'info', summary: 'Info message', text: 'Initial Info Message', dateTime: "2022-01-01" },
+        { severity: 'error', summary: 'Error message', text: 'Ошибка', dateTime: "2022-01-01"},
+        { severity: 'error', summary: 'Error message', text: 'Ошибка', dateTime: "2022-01-01"},
+        { severity: 'error', summary: 'Error message', text: 'Ошибка', dateTime: "2022-01-01"},
+        { severity: 'error', summary: 'Error message', text: 'Ошибка', dateTime: "2022-01-01"}
+      ],
     };
-  }
+  },
+  mounted() {
+    this.op = this.$refs.op;
+  },
+  methods: {
+    toggle(event) {
+      this.op.toggle(event);
+    },
+    showNotification() {
+      this.$refs.toast.add({
+        severity: 'info',
+        summary: 'Cleared Notifications',
+        detail: 'All notifications have been cleared.',
+        life: 3000,
+      });
+    },
+    clearAllMessages() {
+      if (this.notificationMessages.length > 0) {
+        this.notificationMessages = [];
+        this.op.hide();
+        this.showNotification()
+      }
+    },
+  },
 }
 </script>
 
@@ -119,5 +135,16 @@ export default {
   padding-top: 2px;
   padding-bottom: 2px;
   border-radius: 7px;
+}
+.noti-container {
+  max-height: 30rem;
+}
+.notification-body {
+  width: 100%;
+  min-width: 30rem;
+}
+:deep(.p-overlaypanel) {
+  width: 100%;
+  min-width: 30rem!important;
 }
 </style>
